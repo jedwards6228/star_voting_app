20095161eae0121b427b6bc0d9d6a04e775bd025
Jordan Edwards <jedwards6228@gmail.com>
2022-09-03 19:34:36 -0700

    recreating project for better git compatibility

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import star_voting_systems
 from star_voting_systems import Ballot
 from random import randint
 
+"""
+Using random.randint to create a random int
+Need to sudo code how I want to control a ballot's 
+id so I don't submit duplicates and poll id's so I 
+can associate multiple ballots
+"""
+
 current_ballots = ()
 
 
